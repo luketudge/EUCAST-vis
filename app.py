42b5a0300bb5813da188df229b70bc1a866cdd7a
luketudge <luketudge@gmail.com>
2021-02-15 23:12:19 +0100

    minimal working version

---
+++ app.py
@@ -0,0 +1,104 @@
+"""
+Display the EUCAST data.
+"""
+
+import mizani
+import pandas
+import plotnine
+import streamlit
+
+
+DATA_FILEPATH = 'EUCAST_data.csv'
+
+CONCENTRATION_COLS = [
+    '0.002',
+    '0.004',
+    '0.008',
+    '0.016',
+    '0.03',
+    '0.06',
+    '0.125',
+    '0.25',
+    '0.5',
+    '1',
+    '2',
+    '4',
+    '8',
+    '16',
+    '32',
+    '64',
+    '128',
+    '256',
+    '512'
+]
+
+OTHER_COLS = [
+    'ANTIBIOTIC',
+    'SPECIES',
+    'ECOFF',
+    'accessed'
+]
+
+
+#%% wrangle data
+
+d = pandas.read_csv(DATA_FILEPATH)
+
+d = pandas.melt(
+    d,
+    id_vars = OTHER_COLS,
+    value_vars = CONCENTRATION_COLS,
+    var_name = 'concentration',
+    value_name = 'n'
+)
+
+d['concentration'] = d['concentration'].astype(float)
+
+accessed = d['accessed'][0]
+
+
+#%% sidebar
+
+antibiotic = streamlit.sidebar.selectbox(
+    'antibiotic',
+    pandas.unique(d['ANTIBIOTIC'])
+)
+
+d = d[d['ANTIBIOTIC'] == antibiotic]
+
+
+species = streamlit.sidebar.selectbox(
+    'bacteria species',
+    pandas.unique(d['SPECIES'])
+)
+
+d = d[d['SPECIES'] == species]
+
+
+#%% main column
+
+streamlit.title('EUCAST')
+
+streamlit.markdown('accessed ' + accessed)
+
+streamlit.header(antibiotic + ': ' + species)
+
+fig = (
+    plotnine.ggplot(d, plotnine.aes(x = 'concentration', y = 'n'))
+    + plotnine.scale_x_continuous(
+        breaks = pandas.unique(d['concentration']),
+        trans = mizani.transforms.log2_trans
+    )
+    + plotnine.theme(
+        axis_text_x = plotnine.element_text(rotation = 45)
+    )
+    + plotnine.labs(x = 'concentration (mg/L)')
+    + plotnine.geom_col()
+)
+
+streamlit.pyplot(fig.draw())
+
+
+#%% data (for debugging)
+
+#streamlit.dataframe(d)
