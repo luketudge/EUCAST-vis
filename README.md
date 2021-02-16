10a2702c076b243fa0fe8654af446f06914417c2
luketudge <luketudge@gmail.com>
2021-02-16 13:07:59 +0100

    added various flourishes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# EUCAST-vis
+# EUCAST data visualization
+
+An app built with [streamlit](https://www.streamlit.io) and [plotnine](https://plotnine.readthedocs.io/en/stable) to display the [EUCAST data](https://www.eucast.org/mic_distributions_and_ecoffs) on antibiotic resistance.
+
--- app.py
+++ app.py
@@ -36,6 +36,8 @@ CONCENTRATION_COLS = [
 OTHER_COLS = [
     'ANTIBIOTIC',
     'SPECIES',
+    'Distributions',
+    'Observations',
     'ECOFF',
     'ECOFF_tentative',
     'accessed'
@@ -77,16 +79,45 @@ species = streamlit.sidebar.selectbox(
 d = d[d['SPECIES'] == species]
 
 
+accessed = d['accessed'].iloc[0]
+
+n_distributions = d['Distributions'].iloc[0]
+n_observations = d['Observations'].iloc[0]
+
+distributions_text = 'distribution'
+observations_text = 'observation'
+if n_distributions > 1:
+    distributions_text = distributions_text + 's'
+if n_observations > 1:
+    observations_text = observations_text + 's'
+info_text = f'{n_distributions} {distributions_text}, {n_observations} {observations_text}'
+
+ecoff = d['ECOFF'].iloc[0]
+ecoff_missing = numpy.isnan(ecoff)
+ecoff_tentative = d['ECOFF_tentative'].iloc[0]
+
+
+show_ecoff = streamlit.sidebar.checkbox('ECOFF')
+
+if show_ecoff:
+    if ecoff_missing:
+        streamlit.sidebar.markdown('*no ECOFF value available*')
+    elif ecoff_tentative:
+        streamlit.sidebar.markdown(
+                '*ECOFF is tentative (based on fewer than 5 distributions)*'
+        )
+
+
 #%% main column
 
 streamlit.title(antibiotic + ': ' + species)
 
 streamlit.markdown(
-    'source: [EUCAST](https://mic.eucast.org) accessed ' + d['accessed'].iloc[0]
+    'source: [EUCAST](https://mic.eucast.org) accessed ' + accessed
 )
 
 fig = (
-    plotnine.ggplot(d, plotnine.aes(x = 'concentration', y = 'n', fill = 'strain'))
+    plotnine.ggplot(d, plotnine.aes(x = 'concentration', y = 'n'))
     + plotnine.scale_x_continuous(
         breaks = pandas.unique(d['concentration']),
         trans = mizani.transforms.log2_trans
@@ -96,17 +127,37 @@ fig = (
         limits = ['wildtype', 'resistant']
     )
     + plotnine.theme(
-        axis_text_x = plotnine.element_text(rotation = 45)
+        axis_text_x = plotnine.element_text(rotation = 45),
+        legend_position = (0.25, 0.75)
     )
-    + plotnine.labs(x = 'concentration (mg/L)')
+    + plotnine.labs(
+        title = info_text,
+        x = 'concentration (mg/L)'
+    )
     + plotnine.geom_col()
 )
 
+if show_ecoff:
+    fig = (
+        fig
+        + plotnine.aes(fill = 'strain')
+        + plotnine.geom_vline(xintercept = ecoff * 1.5, linetype = 'dashed')
+    )
 
 streamlit.pyplot(fig.draw())
 
-if not numpy.isnan(d['ECOFF'].iloc[0]) and d['ECOFF_tentative'].iloc[0]:
-    streamlit.markdown('NOTE: epidemiological cut-off value is tentative (based on fewer than 5 distributions)')
+streamlit.subheader('Note on limitations')
+streamlit.markdown(
+    'Different studies often use different MIC concentration ranges. \
+    Distributions truncated at the lower end of the scale \
+    within the putative wild-type distribution have been excluded. \
+    The accepted distributions include studies from a wide variety of sources \
+    and time periods and some, on purpose, include high and others low, \
+    proportions of resistant organisms, so the distributions must not be used \
+    to represent rates of resistance to any agent and cannot be used \
+    to compare resistance rates among agents, resistance over time or resistance \
+    in different geographic locations.'
+)
 
 
 #%% data (for debugging)
